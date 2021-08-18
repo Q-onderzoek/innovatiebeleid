8ce4b0f79f219bb1de4cef406b3ff088202e6fc0
Q-onderzoek <89088143+Q-onderzoek@users.noreply.github.com>
2021-08-18 10:03:27 +0200

    Update firebaseInfo.js

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,13 +1,13 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBe-HkqLee_PzPWJ1WUhsKbDFAogQ06xaI",
+	apiKey: "AIzaSyBe-HkqLee_PzPWJ1WUhsKbDFAogQ06xaI",
     authDomain: "q-onderzoek.firebaseapp.com",
     databaseURL: "https://q-onderzoek-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "q-onderzoek",
     storageBucket: "q-onderzoek.appspot.com",
     messagingSenderId: "355263352548",
-    appId: "1:355263352548:web:cfbec57d2804c91e2126f1",
-    measurementId: "G-TECVSJM2J2"
+    appId: "1:355263352548:web:9eaa17a6a1f75d092126f1",
+    measurementId: "G-MV37HD94JY"
 };
 firebase.initializeApp(config);
 var rootRef = firebase.database().ref();
