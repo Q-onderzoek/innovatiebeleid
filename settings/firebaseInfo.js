c4334ed7397ad52ab8a363a31bd94b803c915a96
Q-onderzoek <89088143+Q-onderzoek@users.noreply.github.com>
2021-08-18 11:30:40 +0200

    Create firebaseInfo.js

---
+++ settings/firebaseInfo.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAnIhi-5qUxl9an1XMgfDiNY2eIdaD0qq4",
+  authDomain: "q-study.firebaseapp.com",
+  databaseURL: "https://q-study-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "q-study",
+  storageBucket: "q-study.appspot.com",
+  messagingSenderId: "462639363378",
+  appId: "1:462639363378:web:8910bc839850bb9e57532b",
+  measurementId: "G-3SZRCDG1C4"
+};
+firebase.initializeApp(config);
+var rootRef = firebase.database().ref();
