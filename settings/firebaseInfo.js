42a378de8994e16ef59d59d249691787cb3b37d8
Q-onderzoek <89088143+Q-onderzoek@users.noreply.github.com>
2021-08-18 11:43:58 +0200

    Update firebaseInfo.js

--- settings/firebaseInfo.js
+++ settings/firebaseInfo.js
@@ -1,41 +1,14 @@
 // Initialize Firebase
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js"></script>
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-analytics.js"></script>
+var config = {
+apiKey: "AIzaSyCuRIdG-GQoOhsIMylDAsUDkgBixoypYJA",
+authDomain: "qstudy-cac5b.firebaseapp.com",
+databaseURL: "https://qstudy-cac5b-default-rtdb.firebaseio.com",
+projectId: "qstudy-cac5b",
+storageBucket: "qstudy-cac5b.appspot.com",
+messagingSenderId: "767354644619",
+appId: "1:767354644619:web:9b451784c19146515dcc8e",
+measurementId: "G-L8SCLBZGLJ"
+};
+firebase.initializeApp(config);
+var rootRef = firebase.database().ref();
 
-
-<script>
-
-  // Your web app's Firebase configuration
-
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-  var firebaseConfig = {
-
-    apiKey: "AIzaSyAnIhi-5qUxl9an1XMgfDiNY2eIdaD0qq4",
-
-    authDomain: "q-study.firebaseapp.com",
-
-    databaseURL: "https://q-study-default-rtdb.europe-west1.firebasedatabase.app",
-
-    projectId: "q-study",
-
-    storageBucket: "q-study.appspot.com",
-
-    messagingSenderId: "462639363378",
-
-    appId: "1:462639363378:web:8910bc839850bb9e57532b",
-
-    measurementId: "G-3SZRCDG1C4"
-
-  };
-
-  // Initialize Firebase
-
-  firebase.initializeApp(firebaseConfig);
-
-  firebase.analytics();
-
-</script>
